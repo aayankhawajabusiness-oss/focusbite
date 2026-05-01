a7cec0f3a59bf9dcdf8f4ad406a8a287265df64f
aayankhawajabusiness-oss <aayankhawaja.business@gmail.com>
2026-05-01 07:50:37 -0700

    Delete Index.tsx

--- Index.tsx
+++
@@ -1,8 +0,0 @@
-import { useProfile } from "@/lib/store";
-import OnboardingScreen from "@/components/OnboardingScreen";
-import HomePage from "@/pages/HomePage";
-
-export default function Index() {
-  const { profile } = useProfile();
-  return profile.onboarded ? <HomePage /> : <OnboardingScreen />;
-}
